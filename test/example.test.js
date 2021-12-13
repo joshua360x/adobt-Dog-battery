419fba83a25957118122a8b505abe0febd483e83
joshua360x <jkw5499@psu.edu>
2021-12-13 18:32:32 -0500

    added more TDD test cases

--- test/example.test.js
+++ test/example.test.js
@@ -25,6 +25,13 @@ test('time to test a function', (expect) => {
         age: 14,
         id: 2
     };
+    const puppyComplex = {
+        name: 'wumbo That Barks',
+        breed: 'dachsund',
+        description: 'He barks when he speak Spanish, and understands when you speak the language of the known',
+        age: 24,
+        id: 5
+    };
 
 
     const expected = `<a href="./detail/?id=${dog.id}"><div class="dog-card"><p>${dog.name}</p><img src="./assets/${dog.breed}.jpeg"></div></a>`;
@@ -39,13 +46,17 @@ test('time to test a function', (expect) => {
     
     //test RenderDogDetail
     const expected2 = `<div class="dog-detail"><p class="name">wumbo That Speaks</p><img src="../assets/${dogComplex.breed}.jpeg"><div class="age-and-breed"><p class="age">14 years old</p><p class="breed">chihuahua</p></div><p class="description">He barks when he speak English, and understands when you speak the language of the unknown</p></div>`;
+
+    const expected3 = `<div class="dog-detail"><p class="name">wumbo That Barks</p><img src="../assets/${puppyComplex.breed}.jpeg"><div class="age-and-breed"><p class="age">24 years old</p><p class="breed">dachsund</p></div><p class="description">He barks when he speak Spanish, and understands when you speak the language of the known</p></div>`;
     
     
     const actual2 = renderDogDetail(dogComplex);
+    const actual3 = renderDogDetail(puppyComplex);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected, 'Dog should return an div, p, and img tag nested within an a tag');
     expect.equal(actual1.outerHTML, expected1, 'Puppy should return an div, p, and img tag nested within an a tag');
     expect.equal(actual2.outerHTML, expected2, 'Should return a div with a class of dog detail and its children should be a p tag with a class of name of description and its sibling should be an image tag and div tag with a class of age-and-breed and another sibling should be a p. The div tag with a class of age-and-breed should have two children p tags, one with a class of age and the other breed respectively.');
+    expect.equal(actual3.outerHTML, expected3, 'Should return a div with a class of dog detail and its children should be a p tag with a class of name of description and its sibling should be an image tag and div tag with a class of age-and-breed and another sibling should be a p. The div tag with a class of age-and-breed should have two children p tags, one with a class of age and the other breed respectively.');
 });
