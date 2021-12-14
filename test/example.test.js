821f1de25de84a509ebbfd878f0b4f18eea8f11d
joshua360x <jkw5499@psu.edu>
2021-12-13 20:55:10 -0500

    added TDD for getDog function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 import { renderDogCard, renderDogDetail } from '../render-utils.js';
+import { getDog } from '../fetch-utils.js';
 
 const test = QUnit.test;
 
@@ -53,10 +54,37 @@ test('time to test a function', (expect) => {
     const actual2 = renderDogDetail(dogComplex);
     const actual3 = renderDogDetail(puppyComplex);
 
+
+
+
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected, 'Dog should return an div, p, and img tag nested within an a tag');
     expect.equal(actual1.outerHTML, expected1, 'Puppy should return an div, p, and img tag nested within an a tag');
     expect.equal(actual2.outerHTML, expected2, 'Should return a div with a class of dog detail and its children should be a p tag with a class of name of description and its sibling should be an image tag and div tag with a class of age-and-breed and another sibling should be a p. The div tag with a class of age-and-breed should have two children p tags, one with a class of age and the other breed respectively.');
     expect.equal(actual3.outerHTML, expected3, 'Should return a div with a class of dog detail and its children should be a p tag with a class of name of description and its sibling should be an image tag and div tag with a class of age-and-breed and another sibling should be a p. The div tag with a class of age-and-breed should have two children p tags, one with a class of age and the other breed respectively.');
+
+
+
+});
+
+
+
+test('time to test a function', async(expect) => {
+
+    const expected4 = 
+{
+    'age': 5,
+    'breed': 'corgi',
+    'created_at': '2021-11-18T22:50:36+00:00',
+    'description': 'Borkf I am bekom fat snoot shoober tungg, mlem what a nice floof noodle horse. Heckin good boys you are doin me a concern many pats long woofer wrinkler dat tungg tho blop, big ol adorable doggo tungg fluffer. Puggo thicc length boy such treat, thicc. Length boy h*ck yapper tungg blop, woofer borkdrive smol. Maximum borkdrive such treat porgo you are doing me the shock maximum borkdrive, I am bekom fat thicc.',
+    'id': 2,
+    'name': 'Boogie'
+};
+
+    const actual4 = await getDog(2);
+
+
+    expect.deepEqual(actual4, expected4);
+
 });
